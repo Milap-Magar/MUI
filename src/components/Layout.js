3187169f23541ab6641add694b09db2dbc4f7a7f
milap-magar <milapmagar12@gmail.com>
2024-11-05 21:21:29 +0545

    Create navigate added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Drawer, Typography } from "@mui/material";
+import { Button, Drawer, IconButton, Typography } from "@mui/material";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import Create from "../pages/Create";
 
 const Wrapper = styled.div`
   width: 100%;
 `;
 
 export const Layout = ({ children }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/create");
+  };
   return (
     <div>
       {/* Side bar */}
       <Drawer>
         <div>
           <Typography variant="h5">Note App</Typography>
+          <Button onClick={handleNavigate}>
+            Create
+          </Button>
         </div>
       </Drawer>
       {/* Main section */}
