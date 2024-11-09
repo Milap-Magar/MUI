ca89924b05026fd1cc08b4b1bbb892b21cb96699
milap-magar <milapmagar12@gmail.com>
2024-11-09 21:22:56 +0545

    SNACKBAR

---
+++ src/components/SnackBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Snackbar, Button, Alert } from '@mui/material';
+
+function SnackbarExample() {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClick}>
+        Show Snackbar
+      </Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
+
